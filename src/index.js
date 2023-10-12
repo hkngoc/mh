043f300b79e891c728f8d08ffcaeb758fdae93a0
Hiep Nguyen <hkngoc2@gmail.com>
2023-10-12 22:52:08 +0700

    some init routing

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+
 import { Provider } from 'react-redux';
+
 import { store } from './app/store';
 import App from './App';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
 const container = document.getElementById('root');
